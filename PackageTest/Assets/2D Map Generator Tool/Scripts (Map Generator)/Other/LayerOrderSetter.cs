cc3a0a0d00f4d55278cabb216b44f7450ef9a80d
Zeds_Xd <homenkor24@gmail.com>
2024-03-13 13:01:45 +0300

    Change Map

--- PackageTest/Assets/2D Map Generator Tool/Scripts (Map Generator)/Other/LayerOrderSetter.cs
+++ PackageTest/Assets/2D Map Generator Tool/Scripts (Map Generator)/Other/LayerOrderSetter.cs
@@ -6,7 +6,7 @@ public class LayerOrderSetter : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     private MapGeneratorTool mapGeneratorTool;
 
-    void Start()
+    private void Start()
     {
         mapGeneratorTool = FindObjectOfType<MapGeneratorTool>();
         spriteRenderer.sortingOrder = (int)(mapGeneratorTool.height - transform.position.z);
---
+++ PackageTest/Assets/Scripts/EffectMouseClick/ClickEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickEffect : MonoBehaviour
+{
+    private const float TimeDestroyObject = 2f;
+
+    [SerializeField] private GameObject _effectPrefab;
+
+    private void Update()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+            ClickEffector(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    private void ClickEffector(Vector2 position)
+    {
+        GameObject gameObject = Instantiate(_effectPrefab, position, Quaternion.identity);
+        Destroy(gameObject, TimeDestroyObject);
+    }
+}
