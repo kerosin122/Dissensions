b11473b6347cf081adfde18c793cbd0ee3bdfa12
Zeds_Xd <homenkor24@gmail.com>
2024-03-14 09:32:00 +0300

    Change Inventory

---
+++ PackageTest/Assets/Scripts/Inventory/InventoryPanel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ScriptsInventory
+{
+    public class InventoryPanel : MonoBehaviour
+    {
+        [SerializeField] private GameObject _inventoryPanel;
+        [SerializeField] private List<InventorySlot> _slots;
+        [SerializeField] private Transform _inventoryTransform;
+
+        private bool _isOpen;
+
+        private void Start()
+        {
+            for (int index = 0; index < _inventoryTransform.childCount; index++)
+            {
+                if (_inventoryTransform.GetChild(index).GetComponent<InventorySlot>() != null)
+                    _slots.Add(_inventoryTransform.GetChild(index).GetComponent<InventorySlot>());
+            }
+        }
+
+        private void Update() => OpenOrClosePanel();
+
+        private void OpenOrClosePanel()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _isOpen = !_isOpen;
+
+                _inventoryPanel.SetActive(_isOpen);
+            }
+        }
+
+        public void CloseInventoryPanelButton() => _inventoryPanel.SetActive(false);
+    }
+}
--- PackageTest/Assets/Scripts/Player/Move/PlayerInputMovement.cs
+++ PackageTest/Assets/Scripts/Player/Move/PlayerInputMovement.cs
@@ -20,5 +20,5 @@ namespace ScriptsPlayer
                 OnMoving?.Invoke();
             }
         }
-    }
+    }   
 }
