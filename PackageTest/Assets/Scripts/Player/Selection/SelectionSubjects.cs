6a986e585b625b3f225675ac74c6d8378d2fc01c
Zeds_Xd <homenkor24@gmail.com>
2024-04-08 19:29:04 +0300

    Доделал анимацию подбора.

--- PackageTest/Assets/Scripts/Player/Selection/SelectionSubjects.cs
+++ PackageTest/Assets/Scripts/Player/Selection/SelectionSubjects.cs
@@ -9,31 +9,34 @@ namespace ScriptsPlayer
     public class SelectionSubjects : MonoBehaviour
     {
         private readonly int AppearancePanel = Animator.StringToHash(nameof(AppearancePanel));
+        private readonly int AppearanceAmountItem = Animator.StringToHash(nameof(AppearanceAmountItem));
 
-        [SerializeField] private Animator _animator;
+        [SerializeField] private Animator _animatorInformationProduct;
+        [SerializeField] private Animator _animatorAddAmountItem;
         [SerializeField] private Image _iconProduct;
         [SerializeField] private TextMeshProUGUI _hintText;
         [SerializeField] private InventoryPanel _inventoryPanel;
 
-        private void OnCollisionEnter2D(Collision2D collision) => Lifting(collision);
+        private void OnCollisionEnter2D(Collision2D collision) => LiftingItem(collision);
 
-        private void Lifting(Collision2D collision)
+        private void LiftingItem(Collision2D collision)
         {
             if (collision.gameObject.TryGetComponent(out Weapon item))
             {
                 _inventoryPanel.AddItem(collision.gameObject.GetComponent<Weapon>().Item, collision.gameObject.GetComponent<Weapon>().Amount);
+
                 Destroy(collision.gameObject);
 
                 DrawInformationProduct(item);
-
-                _animator.SetTrigger(AppearancePanel);
+                _animatorInformationProduct.SetTrigger(AppearancePanel);
+                _animatorAddAmountItem.SetTrigger(AppearanceAmountItem);
             }
         }
 
         private void DrawInformationProduct(Weapon item)
         {
-            _hintText.text = $"You have picked up:   {item.Item.NameItem}";
             _iconProduct.sprite = item.Item.Icon;
+            _hintText.text = $"You have picked up:   {item.Item.NameItem}";
         }
     }
 }
